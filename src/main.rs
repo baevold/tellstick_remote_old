dcf8f373c8577ebbb3bfd71450586501816f9b09
wiguy <baekkevold@gmail.com>
2015-06-27 19:07:40 +0200

    Added libc dependency again. Now using stable compiler. Added basic file read in main. Added rpi so file.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-#![feature(libc)]
+//#![feature(libc)]
 
 mod main_test;
 mod telldus;
 extern crate libc;
+
+use std::fs::File;
+use std::io::prelude::*;
+
+static CONFIGFILE: &'static str = "cnf/report.json";
 
 pub mod main {
 	pub fn retvalue() -> i32 {
@@ -12,8 +17,19 @@ pub mod main {
 
 #[allow(dead_code)]
 fn main() {
-	println!("Hello, world! {0}", main::retvalue());
 	telldus::init();
+
+	let mut file = match File::open(CONFIGFILE) {
+		Ok(file) => file,
+		Err(_) => panic!("no such file")
+	};
+	let mut json = String::new();
+	match file.read_to_string(&mut json) {
+		Ok(file) => file,
+		Err(_) => panic!("could not read file to string")
+	};
+	println!("{}", json);
+
 	let sensors = telldus::get_sensors();
 	for sensor in sensors {
 		println!("{}", sensor.to_string());

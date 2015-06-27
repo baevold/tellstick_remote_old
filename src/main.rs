2898f652e139d9e92c311a8f50694d4e800c3961
wiguy <baekkevold@gmail.com>
2015-06-27 20:00:38 +0200

    Added read and write config

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@
 mod main_test;
 mod telldus;
 extern crate libc;
+extern crate rustc_serialize;
 
 use std::fs::File;
 use std::io::prelude::*;
+use rustc_serialize::json::{self};
 
 static CONFIGFILE: &'static str = "cnf/report.json";
 
@@ -15,20 +17,17 @@ pub mod main {
 	}
 }
 
+#[derive(RustcEncodable, RustcDecodable)]
+pub struct Config {
+	clients: Vec<String>
+}
+
 #[allow(dead_code)]
 fn main() {
 	telldus::init();
 
-	let mut file = match File::open(CONFIGFILE) {
-		Ok(file) => file,
-		Err(_) => panic!("no such file")
-	};
-	let mut json = String::new();
-	match file.read_to_string(&mut json) {
-		Ok(file) => file,
-		Err(_) => panic!("could not read file to string")
-	};
-	println!("{}", json);
+	let config = read_config();
+	//write_config();
 
 	let sensors = telldus::get_sensors();
 	for sensor in sensors {
@@ -41,6 +40,27 @@ fn main() {
 	telldus::close();
 }
 
+#[allow(dead_code)]
+fn write_config() {
+	let clients = [String::from("baekkevold.net:8876")];
+	let config = Config{ clients: clients.to_vec() };
+	let data: String = json::encode(&config).unwrap();
+	println!("use echo '[data]' > jq . to prettyfi");
+	println!("{}", data);
+}
+
+fn read_config() -> Option<Config> {
+	let mut file = match File::open(CONFIGFILE) {
+                Ok(file) => file,
+                Err(_) => panic!("no such file")
+        };
+        let mut json = String::new();
+        match file.read_to_string(&mut json) {
+                Ok(file) => file,
+                Err(_) => panic!("could not read file to string")
+        };
+	return Some(json::decode(&json).unwrap());
+}
 
 #[test]
 fn test_retvalue() {
